6ecb02d69251c06db1bbcb0a143b1f06256edcf9
Camila Becker <camila.becker01@gmail.com>
2021-04-05 12:56:34 -0300

    Initial commit

---
+++ server/src/database/index.js
@@ -0,0 +1,24 @@
+const Sequelize = require("sequelize");
+const dbConfig = require("../config/database");
+
+const Atendimentos = require("../models/Atendimentos");
+const Ocorrencias = require("../models/Ocorrencias");
+const Checklist = require("../models/Checklist");
+const Usuarios = require("../models/Usuarios");
+const SolicitarChecklist = require("../models/SolicitarChecklist");
+const Viagem = require("../models/Viagem");
+const DDRModel = require("../models/DDR");
+const EmailFiliais = require("../models/EmailFiliais");
+
+const conexao = new Sequelize(dbConfig);
+
+Atendimentos.init(conexao);
+Ocorrencias.init(conexao);
+Checklist.init(conexao);
+Usuarios.init(conexao);
+SolicitarChecklist.init(conexao);
+Viagem.init(conexao);
+DDRModel.init(conexao);
+EmailFiliais.init(conexao);
+
+module.exports = conexao;
